0d67b1f8b9d4ff6494f69ca734d58160e3ac6ff2
tyxlloyd <48610612+tyxlloyd@users.noreply.github.com>
2020-01-30 17:31:22 -0800

    Added separation between customer and driver requests

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var queue = [];
 
 io.on('connection', socket => {
-    console.log('made socket connection')
+    console.log('made socket connection');
+    io.emit('queue size', queue.length);
     socket.on('ride request', request => {
-        console.log(request);
-        io.emit('ride request', 'You have a new ride request: <coordinates>');
+        queue.push(request);
+        console.log("Requests in queue: " + queue.length);
+        io.emit('queue size', queue.length);
+    }),
+    
+    socket.on('customer request', request => {
+        console.log("A driver has requested a customer");
+        if(queue.length == 0){
+          io.to(request).emit('error', "There aren't any customers waiting. Try again in a bit");
+        }
+        else{
+          var selectedRequest = queue.shift();
+          io.to(request).emit('ride request', 'You have a new ride request: latitude: ' + selectedRequest.lat + '\nlongitude: ' + selectedRequest.long + '\nid: ' + selectedRequest.id);
+          io.to(selectedRequest.id).emit('confirmation', "Your taxi is on the way!");
+          console.log("Requests in queue after shift: " + queue.length);
+        }
     })
   });
 
